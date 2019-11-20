7f9a48c8758fe921cc5ccc2d019671af3a401d70
Keven Carneiro <keven.ncarneiro@gmail.com>
2019-11-20 00:03:14 -0300

    f6861f4

---
+++ precache-manifest.9bddf033a308cec8930d87465b2d7e7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5e9fedee3ddd02eea95",
+    "url": "/pepix-helper/css/about.92fd10bd.css"
+  },
+  {
+    "revision": "d5e9fedee3ddd02eea95",
+    "url": "/pepix-helper/js/about.773b1262.js"
+  },
+  {
+    "revision": "43dbf25bec4258b9d279",
+    "url": "/pepix-helper/css/app.fe27d6a1.css"
+  },
+  {
+    "revision": "43dbf25bec4258b9d279",
+    "url": "/pepix-helper/js/app.59cafaf7.js"
+  },
+  {
+    "revision": "0817f60da3828c9374f3",
+    "url": "/pepix-helper/css/chunk-vendors.225636d4.css"
+  },
+  {
+    "revision": "0817f60da3828c9374f3",
+    "url": "/pepix-helper/js/chunk-vendors.07915aad.js"
+  },
+  {
+    "revision": "2b4974d28dbcea2771a53f90994c730c",
+    "url": "/pepix-helper/img/logo.2b4974d2.png"
+  },
+  {
+    "revision": "412d4e5e404521985d167476d024522d",
+    "url": "/pepix-helper/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/pepix-helper/robots.txt"
+  }
+];
