f6861f443ee806d8e964d35f0ec0209078d6c529
Keven Carneiro <keven.ncarneiro@gmail.com>
2019-11-20 00:02:37 -0300

    Updating

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,30 @@
 <template>
     <div class="about">
-        <h1>Pepix Helper</h1>
-        <p>Este aplicativo tem como função cálculo da Pressão Inspiratória Máxima (PIMax), Pressão Expiratória Maxima
+        <a-card title="Pepix Helper">
+            <p>Este aplicativo tem como função cálculo da Pressão Inspiratória Máxima (PIMax), Pressão Expiratória Maxima
             (PEMax) e porcentagem de força muscular das pressões respiratórias máxima de pacientes adultos do sexo
             feminino e masculino, utilizando informações como idade, sexo e o valor do teste obtido pelo Fisioterapeuta
             e contando com fórmulas descritas na literatura de Neder (1999).</p>
-        <h2>Autores</h2>
-        <ul>
-            <li>Alexa Rodrigues Garcia Santos;</li>
-            <li>Aline Assis Pereira Carvalho;</li>
-            <li>Carolina de Oliveira Sabino;</li>
-            <li>Cíntia Oliveira;</li>
-            <li>Douglas Adão Xavier dos Reis Soares;</li>
-            <li>Keila Machado de Souza;</li>
-            <li>Keven de Nascimento Carneiro;</li>
-            <li>Luiza Cristina Dias Moreira;</li>
-            <li>Rafael Lopes Pereira.</li>
-        </ul>
+        </a-card>
+        <a-card title="Autores">
+            <ul>
+                <li>Alexa Rodrigues Garcia Santos (UNIBH);</li>
+                <li>Aline Assis Pereira Carvalho (UNIBH);</li>
+                <li>Carolina de Oliveira Sabino (UNIBH);</li>
+                <li>Cíntia Oliveira (UNIBH);</li>
+                <li>Douglas Adão Xavier dos Reis Soares (UNIBH);</li>
+                <li>Keila Machado de Souza (UNIBH);</li>
+                <li>Keven de Nascimento Carneiro (UNIFOR);</li>
+                <li>Luiza Cristina Dias Moreira (UNIBH);</li>
+                <li>Rafael Lopes Pereira (UNIBH).</li>
+            </ul>
+        </a-card>
     </div>
 </template>
+
+<style lang="scss" scoped>
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+</style>
